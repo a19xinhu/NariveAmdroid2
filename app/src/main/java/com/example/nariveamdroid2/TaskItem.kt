bffc8b40ec20caf982e71399df7501ebffa50dbe
a19xinhu <63045875+a19xinhu@users.noreply.github.com>
2023-04-19 12:37:17 +0200

    Add TaskItem, Update TaskViewModel

---
+++ app/src/main/java/com/example/nariveamdroid2/TaskItem.kt
@@ -0,0 +1,15 @@
+package com.example.nariveamdroid2
+
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.*
+
+class TaskItem (
+  //  var image: String,
+    var name: String,
+    var desc: String,
+    var dueTime: LocalTime?,
+    var completedDate: LocalDate?,
+    var id: UUID = UUID.randomUUID()
+){
+}
